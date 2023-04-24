675ae3d83fbb26abaf7b618d7144f7679d9f89c6
tosanoob <ctruongtan31070901@gmail.com>
2023-04-24 23:16:35 +0700

    syncing

---
+++ PBL3/Controllers/Admin/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PBL3.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
